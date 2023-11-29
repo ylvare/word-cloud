aebee9ac208d0e7524deab9f4b1846f8173d9832
Ylva Rehnberg <ylva.rehnberg@gmail.com>
2023-11-29 19:42:16 +0100

    new base for front end url

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  base: "/word-cloud/",
   server: {
     proxy: {
       //"/api": { target: { port: 3000, host: "localhost" } },
