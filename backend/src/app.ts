98112918586059fe231482084ea9c4cf5f805cf8
Ylva Rehnberg <ylva.rehnberg@gmail.com>
2023-11-29 21:25:40 +0100

    testing ordinary cors

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -3,26 +3,24 @@ import cors from "cors";
 import textFileRoute from './routes/textFileRoutes.js';
 import rssRoutes from './routes/rssRoutes.js';
 import cloudDataRoute from './routes/cloudDataRoute.js';
-import { isPromise } from "util/types";
-
 //https://github.com/TypeStrong/ts-node/discussions/1781
 
 const app = express();
-//app.use(cors());
+app.use(cors());
 
 // Enable CORS for a specific origin
-app.use(cors({
-  origin: 'https://ylvare.github.io/',
-  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-  credentials: true,
-}));
+// app.use(cors({
+//   origin: 'https://ylvare.github.io/',
+//   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+//   credentials: true,
+// }));
 
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'https://ylvare.github.io/');
-  res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE');
-  res.header('Access-Control-Allow-Headers', 'Content-Type');
-  next();
-});
+// app.use((req, res, next) => {
+//   res.header('Access-Control-Allow-Origin', 'https://ylvare.github.io/');
+//   res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE');
+//   res.header('Access-Control-Allow-Headers', 'Content-Type');
+//   next();
+// });
 
 app.get("/", (req, res) => {
   res.send("Hello World");
