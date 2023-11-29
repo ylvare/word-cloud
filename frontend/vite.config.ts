74d325e91849d2109f07d794e77ddf4bc58a09dd
Ylva Rehnberg <ylva.rehnberg@gmail.com>
2023-11-29 19:31:04 +0100

    cors configuration to production

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,8 +5,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      "/api": { target: { port: 3000, host: "localhost" } },
-      //'/api': { target: 'http://your-production-api.com', changeOrigin: true },
-    },
+      //"/api": { target: { port: 3000, host: "localhost" } },
+      '/api': { target: 'https://word-cloud-mvnj.onrender.com/', changeOrigin: true }},
   },
 });
