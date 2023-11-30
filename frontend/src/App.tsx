8532654dd33e925af528264328d9020507e297e0
Ylva Rehnberg <ylva.rehnberg@gmail.com>
2023-11-30 08:01:14 +0100

    - bugfix width setting to render on mobile - expose host, test on mobile

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -15,10 +15,10 @@ const App: React.FC = () => {
   return (
     <div className="h-screen flex flex-col items-center justify-center space-y-5 bg-gray-100 p-5">
       <h1 className="text-3xl font-bold">Word Cloud Generator</h1>
-      <div className="flex-shrink-0 w-full md:w-3/4 lg:w-3/4 h-96 bg-pink-100 rounded-2xl overflow-auto shadow-md m-5">
+      <div className="w-full md:w-3/4 lg:w-3/4 h-96 bg-pink-100 rounded-2xl overflow-auto shadow-md m-5">
         <WordCloud wordCloudData={wordCloudData} />
       </div>
-      <div className="flex-shrink-0 w-full md:w-1/2 lg:w-1/2">
+      <div className="w-full md:w-1/2 lg:w-1/2">
         <FileAndRssUpload onUploadSuccess={handleUploadSuccess} />
       </div>
     </div>
